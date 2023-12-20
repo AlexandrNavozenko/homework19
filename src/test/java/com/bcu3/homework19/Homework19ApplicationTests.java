0970e14f290554043f0310f1f948008d4b69cf6a
AlexandrNavozenko <alexandr.navozenko.dev@gmail.com>
2023-12-20 10:25:20 +0100

    init

---
+++ src/test/java/com/bcu3/homework19/Homework19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcu3.homework19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework19ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
